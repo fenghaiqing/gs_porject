d8e49a0355a3d73f3c0b4bd829e9aa2c4ec45928
sxc <961207723@qq.com>
2018-06-25 23:56:03 +0800

    update

---
+++ schedule/database/user.js
@@ -0,0 +1,40 @@
+var Sequelize = require('sequelize');
+var sequelize = require('./db');
+var User = sequelize.define('users', {
+    id:{
+        type:Sequelize.INTEGER,
+        primaryKey: true
+    },
+    userName: {
+        type: Sequelize.STRING, // 指定值的类型
+        field: 'user_name' // 指定存储在表中的键名称
+    },
+    // 没有指定 field，表中键名称则与对象键名相同
+    password: {
+        type: Sequelize.STRING
+    }
+}, {
+    // 如果为 true 则表的名称和 model 相同，即 user
+    // 为 false MySQL创建的表名称会是复数 users
+    // 如果指定的表名称本就是复数形式则不变
+    freezeTableName: false,
+    timestamps: false
+});
+var user = User.sync({ force: false });
+// 添加新用户
+exports.addUser = function(userName, email) {
+    // 向 user 表中插入数据
+    return User.create({
+        id:null,
+        userName: userName,
+        password: email
+    });
+};
+// 通过用户名查找用户
+exports.findByName = function(userName) {
+    return User.findAll({ where: { user_name: userName } });
+};
+
+exports.login = function(user) {
+    return User.findOne({ where: user });
+};
--- schedule/routes/users.js
+++ schedule/routes/users.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var user =require('../database/user');
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/',jsonParser, function(req, res, next) {
+   user.findByName(req.query.userName).then(function(users,err){
+       if(users.length!=0){
+           throw {msg:"用户已存在!",code:501};
+       }else{
+           return user.addUser(req.query.userName,req.query.password);
+       }
+   })
+    .then(function(users){
+        res.json(users);
+    }).catch(function(err){
+        res.json(err)
+   });
 });
-
+router.post("/login",jsonParser,function(req,res,next){
+   return user.login(req.body);
+}).then(function(result){
+    if(result!=null&&result!=undefined){
+        res.json(result);
+    }else{
+        res.json({code:501,msg:'用户密码不存在！'})
+    }
+});
 module.exports = router;
